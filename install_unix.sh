d54321fb65d0f85a1bcab527643233d1076eabe6
titfora <titouan821@gmail.com>
2023-09-21 14:40:33 +0200

    add unix udev rules

--- install_unix.sh
+++ install_unix.sh
@@ -55,6 +55,10 @@ then
     echo_color 1 "  e.g. ./install_unix.sh /usr/bin/arduino-cli"
     echo_color 3 "[INFO] You must manually install arduino-cli from your package manager."
 else
+    echo_color 2 "### UDEV RULES INSTALLATION ###"
+    echo_color 3 "[INFO] Needed to give non-root users permission to access the Teensy USB device."
+    sudo curl https://www.pjrc.com/teensy/00-teensy.rules -o /etc/udev/rules.d/00-teensy.rules
+
     echo_color 2 "### ARDUINO-CLI SETUP ###"
     $1 config init
     $1 config add board_manager.additional_urls https://www.pjrc.com/teensy/package_teensy_index.json
@@ -63,6 +67,8 @@ else
 
     # Library install
     $1 lib install Servo
+
+    echo_color 2 "### MAKEFILES SETUP ###"
 
     build_Makefile "./Teensy/Makefile" $1 "teensy:avr:teensy41" "" "-Wno-error=cast-function-type" #:usb=serial,speed=600,opt=o2std,keys=en-us    # opt https://forum.arduino.cc/t/arduino-cli-teesnsy-notes-questions-documentation/1068881/2"
     echo_color 2 "Teensy's Makefile done!"
