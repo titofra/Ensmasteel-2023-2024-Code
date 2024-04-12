d196a9e4be6368a1a3ff15dbe04b68e34fd52a4e
titofra <titouan821@gmail.com>
2024-04-12 15:52:10 +0200

    add background to Sequence_Tester

--- Local/Sequence_Tester/README.md
+++ Local/Sequence_Tester/README.md
@@ -1,16 +1,14 @@
 # Sequence Tester
 
-Code that aims to help in building a sequence. It enable the user to run and print any sequence.
-
-## WARNINGS
-
-This code depends on class created for Teensy's board. However, they depend on uncompatible code with a local compilation (e.g. <Arduino.h>, <Stream.h> ...). This is why necessary code is copied and edited under include/ and src/: **they must be updated** to match with the actual files at Teensy/include/ and Teensy/src/!
-
-## Compilation
-
-You can use any C++ compiler to compile this code. Note that it depends on SFML library (install it from [https://www.sfml-dev.org/download.php](https://www.sfml-dev.org/download.php)), so make sure you specify `-lsfml-graphics -lsfml-window -lsfml-system` flags.
-Example using g++: `g++ -Wall -Wextra -Werror -O3 -Wundef -Wcast-align -Wwrite-strings -Wunreachable-code -Wconversion -Wno-error=maybe-uninitialized -Iinclude/ src/* -lsfml-graphics -lsfml-window -lsfml-system`.
+Program that aims to help in building a sequence. It enables the user to run and print any sequence.
 
 <p align="center">
   <img src="https://github.com/titofra/Ensmasteel-2023-2024-Code/blob/main/Local/Sequence_Tester/resources/example.png"></a>
 </p>
+
+> [!CAUTION]
+> This code depends on class created for Teensy's board. However, they depend on uncompatible code with a local compilation (e.g. <Arduino.h>, <Stream.h> ...). This is why necessary code is copied and edited under include/ and src/: **they must be updated** to match with the actual files at Teensy/include/ and Teensy/src/!
+
+## Dependencies
+
+This program requires SFML which can be install from [https://www.sfml-dev.org/download.php](https://www.sfml-dev.org/download.php).
--- Local/Sequence_Tester/src/sequence_tester.cpp
+++ Local/Sequence_Tester/src/sequence_tester.cpp
@@ -2,22 +2,19 @@
 #include "action.hpp"
 #include "action_functions.hpp"
 #include <SFML/Graphics.hpp>
+#include <nanosvg.h>
+#include <nanosvgrast.h>
 
 std::vector<Kinetic> Get_Kinetics_from_Sequence (Sequence seq, unsigned long dt, Kinetic beginKinetic);
-void Display (std::vector<Kinetic> kinetics, const int width, const int height, float mmToPxX, float mmToPxY, float pxOffsetX, float pxOffsetY, unsigned long dt, float Rpoint = 3.0f, float orient_line_sz = 20.0f);
-
+void Display (std::vector<Kinetic> kinetics, float scale, float pxOffsetX, float pxOffsetY, unsigned long dt, float Rpoint = 4.0f, float orient_line_sz = 20.0f);
 void BuildSequence (Sequence* seq);
 
-const int width = 1600;
-const int height = 800;
-const int pxOffsetX = 10;
-const int pxOffsetY = 10;
-const float mmToPxX = (float) (width - 2 * pxOffsetY) / 3000.0f;
-const float mmToPxY = (float) (height - 2 * pxOffsetX) / 1500.0f;
-const unsigned long dt = 100;    // here dt has ne effect on the trajectory, however it has an effect in the number of sequence's call aka the number of dots on the graph
-
-
 int main (void) {
+    const int pxOffsetX = 10;
+    const int pxOffsetY = 10;
+    const float scale = 0.13f;
+    const unsigned long dt = 100;    // here dt has ne effect on the trajectory, however it has an effect in the number of sequence's call aka the number of dots on the graph
+
     /* create the sequence */
     Sequence seq;
     BuildSequence (&seq);
@@ -27,7 +24,7 @@ int main (void) {
     std::vector<Kinetic> kinetics = Get_Kinetics_from_Sequence (seq, dt, beginKinetic);
 
     /* display the graph */
-    Display (kinetics, width, height, mmToPxX, mmToPxY, pxOffsetX, pxOffsetY, dt);
+    Display (kinetics, scale, pxOffsetX, pxOffsetY, dt);
 
     return 0;
 }
@@ -56,40 +53,68 @@ std::vector<Kinetic> Get_Kinetics_from_Sequence (Sequence seq, unsigned long dt,
     return kinetics;
 }
 
-void Display (std::vector<Kinetic> kinetics, const int width, const int height, float mmToPxX, float mmToPxY, float pxOffsetX, float pxOffsetY, unsigned long dt, float Rpoint, float orient_line_sz) {
-    sf::RenderWindow window(sf::VideoMode(width, height), "Sequence Tester - Ensmasteel");
-    window.setFramerateLimit((unsigned int) (1000 / dt));
+void Display (std::vector<Kinetic> kinetics, float scale, float pxOffsetX, float pxOffsetY, unsigned long dt, float Rpoint, float orient_line_sz) {
+    /* BACKGROUND */
+    NSVGimage* bkg_svg = nsvgParseFromFile ("resources/table.svg", "px", 96);
+    struct NSVGrasterizer* bkg_rast = nsvgCreateRasterizer ();
+    const unsigned int width = (unsigned int) bkg_svg->width;
+    const unsigned int height = (unsigned int) bkg_svg->height;
+    unsigned char* bkg_img = (unsigned char*) malloc (width * height * 4);
+    nsvgRasterize (bkg_rast, bkg_svg, 0, 0, 1, bkg_img, width, height, width * 4);
+    nsvgDeleteRasterizer (bkg_rast);
+    nsvgDelete (bkg_svg);
+    sf::Texture backgroundTexture;
+    backgroundTexture.create (width, height);
+    backgroundTexture.update (reinterpret_cast<const sf::Uint8*> (bkg_img), width, height, 0, 0);
+    sf::Sprite background;
+    background.setTexture (backgroundTexture);
+    background.setPosition (pxOffsetX, pxOffsetY);
+    background.setScale (scale, scale);
+
+    /* WINDOWS */
+    sf::RenderWindow window (
+        sf::VideoMode (
+            (unsigned int) background.getGlobalBounds ().width + 2 * (unsigned int) pxOffsetX,
+            (unsigned int) background.getGlobalBounds ().height + 2 * (unsigned int) pxOffsetY
+        ),
+        "Sequence Tester - Ensmasteel",
+        sf::Style::Titlebar | sf::Style::Close
+    );
+    window.setFramerateLimit ((unsigned int) (1000 / dt));
 
     /* CREATE OBJECTS */
     // graph
-    sf::VertexArray graph(sf::LinesStrip);
+    sf::VertexArray graph (sf::LinesStrip);
 
     //points
     std::vector<sf::CircleShape> points;
-    sf::CircleShape point(Rpoint);
-    point.setFillColor(sf::Color::Green);
+    sf::CircleShape point (Rpoint);
+    point.setFillColor (sf::Color::White);
 
     // orientation indicators
     std::vector<sf::VertexArray> orientations;
+
+    const float mmToPxX = (float) (background.getGlobalBounds ().width - 2.0f * pxOffsetY) / 3000.0f;
+    const float mmToPxY = (float) (background.getGlobalBounds ().height - 2.0f * pxOffsetX) / 1500.0f;
 
     sf::Vector2f position;
     for (const Kinetic& kin : kinetics) {
         position = sf::Vector2f (pxOffsetX + kin.getX () * mmToPxX, pxOffsetY + kin.getY () * mmToPxY);
 
         // graph
-        graph.append (position);
+        graph.append (sf::Vertex (position, sf::Color::Black));
 
         // point
-        point.setPosition(position - sf::Vector2f (Rpoint, Rpoint));
+        point.setPosition (position - sf::Vector2f (Rpoint, Rpoint));
         points.push_back (point);
 
         // orientation indicator
-        sf::VertexArray line(sf::Lines, 2);
-        line.append (sf::Vertex(position, sf::Color::Red));
-        line.append (sf::Vertex(
+        sf::VertexArray line (sf::Lines, 2);
+        line.append (sf::Vertex (position, sf::Color::Blue));
+        line.append (sf::Vertex (
             position
             + sf::Vector2f (orient_line_sz * cos (kin.getTheta ()), orient_line_sz * sin (kin.getTheta ()))
-            , sf::Color::Red
+            , sf::Color::Blue
         ));
         orientations.push_back (line);
     }
@@ -103,26 +128,29 @@ void Display (std::vector<Kinetic> kinetics, const int width, const int height, 
             }
         }
 
-        window.clear(sf::Color::Black);
+        window.clear();
+
+        // background
+        window.draw (background);
 
         // graph
-        sf::VertexArray graphToDraw(sf::LinesStrip);
+        sf::VertexArray graphToDraw (sf::LinesStrip);
         for (int i = 0; i < frame; i++) {
             graphToDraw.append (graph [i]);
         } 
-        window.draw(graphToDraw);
+        window.draw (graphToDraw);
 
         // points
         for (int i = 0; i < frame; i++) {
-            window.draw(points [i]);
+            window.draw (points [i]);
         }
 
         // orientation indicators
         for (int i = 0; i < frame; i++) {
-            window.draw(orientations [i]);
+            window.draw (orientations [i]);
         }
 
-        window.display();
+        window.display ();
 
         frame ++;
         if (frame > (int) (100000 / dt)) frame = (int) (100000 / dt);
